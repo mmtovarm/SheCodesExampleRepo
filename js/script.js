4c1989c80c9d47043ea2800fdcbebcbbe6784138
Juan Carrasco <jcarr259@123-243-154-186.tpgi.com.au>
2023-05-16 20:30:55 +1000

    get started

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburgerButton = document.querySelector(".hamburger-button");
+const mobileNav = documet.querySelector("mobile-nav display none")
+
+hamburgerButton.addEventListener("click",() => {
+  alert("holy moley it's working")
+})
